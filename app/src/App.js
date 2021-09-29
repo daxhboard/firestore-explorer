668a9e7dfd53776c6b9a0087f7963414c8648f8d
ranajahanzaib <iam@ranajahanzaib.com>
2021-09-29 21:00:14 +0500

    Use View Component to Display Single Document Component

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,15 @@
+import SingleDocument from "./components/Document/SingleDocument";
+import View from "./components/View";
 import "./styles/App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <View className="mt-5 mb-5">
+        <SingleDocument />
+      </View>
+    </div>
+  );
 }
 
 export default App;
